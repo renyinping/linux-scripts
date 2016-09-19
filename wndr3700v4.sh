6eeaa7dae3192447d3463cc75292a1a7a34281ca
yinping <yp_ren@hotmail.com>
2016-09-19 13:34:36 +0800

    modify wndr3700v4.sh

--- wndr3700v4.sh
+++ wndr3700v4.sh
@@ -6,7 +6,10 @@ nand128m()
 	OLD='wndr4300_mtdlayout=mtdparts=ar934x-nfc:256k(u-boot)ro,256k(u-boot-env)ro,256k(caldata),512k(pot),2048k(language),512k(config),3072k(traffic_meter),2048k(kernel),23552k(ubi),25600k@0x6c0000(firmware),256k(caldata_backup),-(reserved)'
 	NEW='wndr4300_mtdlayout=mtdparts=ar934x-nfc:256k(u-boot)ro,256k(u-boot-env)ro,256k(caldata),512k(pot),2048k(language),512k(config),3072k(traffic_meter),2048k(kernel),121856k(ubi),123904k@0x6c0000(firmware),256k(caldata_backup),-(reserved)'
 	EDIT_FILE="target/linux/ar71xx/image/Makefile"
-	sed -i "s/${OLD}/${NEW}/g" ${EDIT_FILE};
+#	sed -n  "/^${OLD}$/p" ${EDIT_FILE};
+	sed -i "s/^${OLD}$/${NEW}/g" ${EDIT_FILE};
+#	sed -n  "/^${NEW}$/p" ${EDIT_FILE};
+	[ `sed -n  "/^${NEW}$/p" ${EDIT_FILE} | wc -l` -eq 1 ] && echo OK. ;
 }
 
 ################################################################
