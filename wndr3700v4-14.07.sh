18d2a7a5bf9c20819c02b7eac705b504958dd8a2
yinping <yp_ren@hotmail.com>
2017-01-07 20:15:37 +0800

    ..

--- wndr3700v4-14.07.sh
+++ wndr3700v4-14.07.sh
@@ -6,18 +6,16 @@ PATH=`dirname $(readlink -f $0)`/functions:$PATH
 . download.sh
 . openwrt-build-system.sh
 
+ARCH=ar71xx_nand_1407
+
 install()
 {
 	openwrt_build_system_install
 }
 
 # 软件仓库
-repositories_conf()
+repo()
 {
-	# 缺少参数
-	[ -z "$1" ] && print_error "Missing parameters 1." && return 1
-	local ARCH=$1
-	
 	local FILE="${ARCH}/img/repositories.conf"
 	[ ! -f "${FILE}" ] && print_error "File not found: ${FILE}" && return 1
 	
@@ -37,12 +35,8 @@ src imagebuilder file:packages' > ${FILE}
 
 # 使用全部128M NAND 空间
 # TOP_DIR $1 OpenWrt Buildroot 构建根目录路径
-wndr4300_nand128m()
+nand128m()
 {
-	# 缺少参数
-	[ -z "$1" ] && print_error "Missing parameters 1." && return 1
-	local ARCH=$1
-	
 	local FILE="${ARCH}/img/target/linux/ar71xx/image/Makefile"
 	[ ! -f "${FILE}" ] && print_error "File not found: ${FILE}" && return 1
 	
@@ -55,48 +49,38 @@ wndr4300_nand128m()
 	[ `sed -n  "/^${NEW}$/p" ${FILE} | wc -l` -eq 1 ] && echo "WNDR4300/WNDR3700v4 NAND 128M OK."
 }
 
-# 配置文件
-files()
+# 构建环境
+img_sys()
 {
-	# 缺少参数
-	[ -z "$1" ] && print_error "Missing parameters 1." && return 1
-	local ARCH=$1
-	
-	local FILES_DIR=${ARCH}/img/files
-	local BAK_FILE=backup-openwrt.tar.gz
-	
-	# DEBUG 
-	print_debug "wndr3700v4.sh files $*"
-	print_debug "ARCH=${ARCH}"
-	# End
-	
-	[ -f "${BAK_FILE}" ] \
-		&& mkdir -p "${FILES_DIR}" \
-		&& tar -zxvf "${BAK_FILE}" -C "${FILES_DIR}/"
-}
-
-files_rm()
-{
-	# 缺少参数
-	[ -z "$1" ] && print_error "Missing parameters 1." && return 1
-	local ARCH=$1
-	
-	rm -rf ${ARCH}/img/files/ \
-		&& echo "The deletion is complete."
-}
-
-img1407()
-{
-	local ARCH=ar71xx_nand_1407
 	local MD5=67982a1cfd53133d692d2757a2285a4c
 	local DL_URL=https://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/nand/OpenWrt-ImageBuilder-ar71xx_nand-for-linux-x86_64.tar.bz2
 	local DL_SAVE=dl/OpenWrt-ImageBuilder-ar71xx_nand-for-linux-x86_64.tar.bz2
 	local TOP_DIR=OpenWrt-ImageBuilder-ar71xx_nand-for-linux-x86_64
 	image_build_system ${ARCH} ${MD5} ${DL_URL} ${DL_SAVE} ${TOP_DIR}
-	wndr4300_nand128m ${ARCH}
-	repositories_conf ${ARCH}
-	
-	# packages
+	nand128m
+	repo
+}
+
+# clean
+clean()
+{
+	pushd ${ARCH}/img
+	rm -rf files/ 
+	make clean
+	popd
+}
+
+# 解包备份文件
+files()
+{
+	mkdir -p ${ARCH}/img/files
+	tar -zxvf backup-openwrt.tar.gz -C ${ARCH}/img/files/
+}
+
+# 构建镜像
+img()
+{
+	# 包列表
 	local LUCI="luci luci-i18n-chinese"
 	local TOOLS="wget ca-certificates unzip zip unrar tar bash bash-completion hdparm bind-dig"
 	local USB="kmod-usb-storage block-mount usbutils blkid fdisk"
@@ -106,14 +90,20 @@ img1407()
 	local BT="transmission-daemon luci-app-transmission transmission-web"
 	local SS="shadowsocks-client polipo"
 	local XXNET="python python-openssl pyopenssl wget ca-certificates unzip bash"
-	local ALL="$LUCI $TOOLS $USB $EXT4 $FAT32 $SMB $BT $SS $XXNET"
 	
-	# make image
+	# 基础包和全功能包选择
+	if [ "$1" = "base" ]; then
+		local IPK_LIST="$LUCI -kmod-usb-core -kmod-usb-ohci -kmod-usb2"
+	else
+		local IPK_LIST="$LUCI $TOOLS $USB $EXT4 $FAT32 $SMB $BT $SS $XXNET"
+	fi
+	
+	# make
 	pushd ${ARCH}/img
 	if [ -d "files" ]; then
-		make image PROFILE=WNDR4300 PACKAGES="${ALL}" FILES=files
+		make image PROFILE=WNDR4300 PACKAGES="${IPK_LIST}" FILES=files
 	else
-		make image PROFILE=WNDR4300 PACKAGES="${ALL}"
+		make image PROFILE=WNDR4300 PACKAGES="${IPK_LIST}"
 	fi
 	popd
 }
